c0c549428655095920599af37ae51cb9d3dfc565
James Thorp <jamesthorp1@gmail.com>
2018-11-08 13:48:39 +0000

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/show'
+  get 'cocktails/edit'
+  get 'cocktails/update'
+  get 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :cocktails
 end
