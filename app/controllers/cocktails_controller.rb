72e76e7eb564e6b2c8b35cce883e68607f22462f
James Thorp <jamesthorp1@gmail.com>
2018-11-08 14:40:56 +0000

    doses controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,13 +14,13 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
-def cocktail_params
-params.require(:cocktail).permit(:name)
-end
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
-  get 'cocktails/index'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  get 'cocktails/show'
-  get 'cocktails/edit'
-  get 'cocktails/update'
-  get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
