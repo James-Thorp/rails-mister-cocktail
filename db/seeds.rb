f7fe6de256dbb7bdc99b7b678f3a0e10e6e3b9d3
James Thorp <jamesthorp1@gmail.com>
2018-11-09 16:14:46 +0000

    API added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+Ingredient.destroy_all
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = open(url).read
+ingredients_JSON = JSON.parse(ingredients)
+
+ingredients_JSON["drinks"].each do |ingredient_hash|
+  name = ingredient_hash["strIngredient1"]
+  Ingredient.create!(name: name)
+end
+
+
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
