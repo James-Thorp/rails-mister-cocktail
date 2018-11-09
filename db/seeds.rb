0e03c996ff88d4b0aac1627ee18d9588114809ef
James Thorp <jamesthorp1@gmail.com>
2018-11-09 22:13:23 +0000

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'json'
-require 'open-uri'
-Ingredient.destroy_all
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients = open(url).read
-ingredients_JSON = JSON.parse(ingredients)
+# require 'json'
+# require 'open-uri'
+# Ingredient.destroy_all
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients = open(url).read
+# ingredients_JSON = JSON.parse(ingredients)
 
-ingredients_JSON["drinks"].each do |ingredient_hash|
-  name = ingredient_hash["strIngredient1"]
-  Ingredient.create!(name: name)
-end
+# ingredients_JSON["drinks"].each do |ingredient_hash|
+#   name = ingredient_hash["strIngredient1"]
+#   Ingredient.create!(name: name)
+# end
+Ingredient.destroy_all
 
 
 
